47457cd18aa064d022131f82a5460b4812841a86
PlotTwist <84284643+Swardcrafter@users.noreply.github.com>
2023-11-01 16:02:34 -0700

    1

--- commands/ping.py
+++ commands/ping.py
@@ -1,17 +1,12 @@
 from discord.ext import commands
 
 class Ping(commands.Cog):
-	def __init__(self, client):
-		self.client = client # sets the client variable so we can use it in cogs
-	
-	@commands.Cog.listener()
-	async def on_ready(self):
-		print("Ready")
-	
-	@commands.command()
-	async def command(self, ctx):
-		print("Ping Run.")
-		await ctx.send("Pong")
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.hybrid_command()
+    async def ping(self, ctx):
+        await ctx.reply(f"Pong")
 
-def setup(client):
-	client.add_cog(Ping(client))
+async def setup(client):
+    await client.add_cog(Ping(client))
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import os
 import json
@@ -6,14 +7,27 @@ import json
 with open('secrets.json', 'r') as json_file:
     secrets = json.load(json_file)
 
+class Client(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix = secrets["COMMAND_PREFIX"],
+            intents = discord.Intents.all(),
+            help_command = commands.DefaultHelpCommand(dm_help=True)
+        )
+    
+    async def setup_hook(self): #overwriting a handler
+        print(f"\033[31mLogged in as {client.user}\033[39m")
+        cogs_folder = f"{os.path.abspath(os.path.dirname(__file__))}/commands"
+        for filename in os.listdir(cogs_folder):
+            if filename.endswith(".py"):
+                await client.load_extension(f"commands.{filename[:-3]}")
+        await client.tree.sync()
+        print("Loaded cogs")
 
+client = Client()
 
-client = commands.Bot(command_prefix = secrets["COMMAND_PREFIX"])
-
-for f in os.listdir("./commands"):
-	if f.endswith(".py"):
-		client.load_extension("cogs." + f[:-3])\
+@client.event
+async def on_ready():
+    print("Bot is ready")
 
 client.run(secrets["TOKEN"])
-
-print("Running")
