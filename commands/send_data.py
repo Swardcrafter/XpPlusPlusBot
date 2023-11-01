29e9fe8b4cdba650c97572972f64ed9fa5efcbba
PlotTwist <84284643+Swardcrafter@users.noreply.github.com>
2023-11-01 16:13:34 -0700

    1

---
+++ commands/send_data.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+import requests
+import os
+
+class SendData(commands.Cog):
+	def __init__(self, client):
+		self.client = client
+    
+	@commands.hybrid_command()
+	async def send_data(self, ctx, data: str):
+		response = requests.post(os.environ['FLASK_URL'], json=data)
+
+		if response.status_code == 200:
+			await ctx.send("Message sent successfully")
+		else:
+			await ctx.send("Failed to send message")
+		await ctx.reply(f"Send {data}")
+
+async def setup(client):
+    await client.add_cog(SendData(client))
